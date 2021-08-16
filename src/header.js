f18321db8d9b908f9e24e883149bb211dc2b28a6
Dan Tuls <danltuls@gmail.com>
2021-08-16 13:34:42 -0400

    header.js created

---
+++ src/header.js
@@ -0,0 +1,8 @@
+const Header = () => {
+  <header className="row">
+    <div className="col-md-5">
+      <img src="https://journalists.medianet.com.au/DisplayAttachment.aspx?t=38022&file-type=thumb" className="logo" />
+    </div>
+    <div className="col-md-7 mt-5 subtitle">Get your time back!</div>
+  </header>;
+};
